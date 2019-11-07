f35b875a9be13305c9acab03a8ebcbd7991161a1
Oscar García <54967287+OscarGarcia98@users.noreply.github.com>
2019-11-06 22:01:52 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,20 +11,9 @@ namespace Autenticacion
        
         static void Main(string[] args)
         {
-            Principal p = new Principal();
-            Login log = new Login();
-            log.Log();
-            // p.Menu();
-
-           // string nombre = "Oscar Daniel";
-
-           // string[] datos = nombre.Split(' ');
-
-           // foreach(var item in datos)
-            //{
-           //     Console.WriteLine(item);
-           // }
-           // Console.ReadKey();
+            Principal p = new Principal();       
+         
+             p.Menu();
 
 
         }
